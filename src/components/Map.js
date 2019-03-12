89855e303f0d27b868e353aa3893fba95d7824f2
Allison Lucas <allisonlucas@Allisons-MacBook-Pro-2.local>
2019-03-12 17:00:47 -0600

    can view a single route

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { parseJson } from '../utils/parsers';
 import EasyFit from 'easy-fit';
+import { geoMercator, geoPath } from 'd3-geo';
+import geojsonExtent from '@mapbox/geojson-extent';
+import './map.css';
 
 class Map extends Component {
     constructor(props) {
@@ -41,6 +44,7 @@ class Map extends Component {
                 if (error) {
                     console.error('Error parsing .fit file: ', error);
                 } else {
+                    console.log('data: ', data)
                     this.setState({jsonData: data });
                 }
             });
@@ -50,14 +54,40 @@ class Map extends Component {
     };
 
     render () {
+        const { geoJson } = this.state;
+
+        let routes;
+        if (geoJson) {
+            const extent = geojsonExtent(geoJson);
+            const extentFormatted = [[extent[0], extent[1]], [extent[2], extent[3]]]
+            const projection = geoMercator()
+                .center([-105.6783867,39.1881631])
+                .fitExtent(extentFormatted, geoJson)
+                .scale(50000)
+                .translate([ 0, 1050 ]);
+            const pathGenerator = geoPath().projection(projection);
+            routes = geoJson.features.map((d,i) => (
+                <path
+                    key={'path' + i}
+                    d={pathGenerator(d)}
+                    className='routes'
+                />
+            ));
+        }
+
         return (
-            <form onSubmit={this.handleFileUpload}>
-                <span>Select a .fit file provided by Strava (or Garmin).</span>
-                <br/>
-                <input type="file" id="file" name="file" encType="multipart/form-data" ref={this.fileInput}/>
-                <br/>
-                <button type="submit">Submit</button>
-            </form>
+            <div>
+                <form onSubmit={this.handleFileUpload}>
+                    <span>Select a .fit file provided by Strava (or Garmin).</span>
+                    <br/>
+                    <input type="file" id="file" name="file" encType="multipart/form-data" ref={this.fileInput}/>
+                    <br/>
+                    <button type="submit">Submit</button>
+                </form>
+                { routes && <svg width={500} height={500}>
+                    {routes}
+                </svg> }
+            </div>
         )
     }
 }
--- src/utils/parsers.js
+++ src/utils/parsers.js
@@ -25,4 +25,40 @@ export const parseJson = json => {
         });
     }
     return geoJson;
+
+    // const geoJson = {
+    //     type: "FeatureCollection",
+    //     features: [],
+    // };
+
+    // geoJson.features.push({
+    //     "type": "Feature",
+    //     "geometry": {
+    //         "type": "LineString",
+    //         "coordinates": []
+    //     },
+    //     "properties": {
+    //         "avg_power": json.sessions[0].avg_power,
+    //         "avg_speed": json.sessions[0].avg_speed,
+    //         "intensity_factor": json.sessions[0].intensity_factor,
+    //         "max_power": json.sessions[0].max_power,
+    //         "max_speed": json.sessions[0].max_speed,
+    //         "normalized_power": json.sessions[0].normalized_power,
+    //         "sport": json.sessions[0].sport,
+    //         "start_time": json.sessions[0].start_time,
+    //         "total_ascent": json.sessions[0].total_ascent,
+    //         "total_calories": json.sessions[0].total_calories,
+    //         "total_descent": json.sessions[0].total_descent,
+    //         "total_distance": json.sessions[0].total_distance,
+    //         "total_elapsed_time": json.sessions[0].total_elapsed_time,
+    //         "total_timer_time": json.sessions[0].total_timer_time,
+    //         "training_stress_score": json.sessions[0].training_stress_score
+    //     }
+    // })
+      
+    // for (let i = 0; i < json.records.length; i++) {
+    //     geoJson.features[0].geometry.coordinates.push(json.records[i].position_long, json.records[i].position_lat);
+    // }
+    // console.log('geojson: ', geoJson)
+    // return geoJson;
 }
